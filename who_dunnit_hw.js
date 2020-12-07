dc148720eb017f545d997836404a6a301120931f
William Anderson <willjatlas@googlemail.com>
2020-12-07 20:38:47 +0000

    finished mvp

--- who_dunnit_hw.js
+++ who_dunnit_hw.js
@@ -64,7 +64,7 @@ that changes the value of 'murderer', it is a constant variable and can not be c
 /* Prediction - firstVerdict  = " The murderer is Mrs. Peacock. "
                 secondVerdict = " The murderer is Professor Plum. "
 This is because the 'let murderer' variable within the declareMurderer function is not
-accessable globally. When we call the 'murderer' variable in the string literal for the 
+accessible globally. When we call the 'murderer' variable in the string literal for the 
 secondVerdict assignment, it call only see the initial 'let murderer' variable initialised 
 at the beginning. 
 */
@@ -87,7 +87,7 @@ at the beginning.
 /* Prediction - "The suspects are Miss Scarlet, Professor Plum, Colonel Mustard."
                 "Suspect three is Mrs. Peacock."
 This is again because the suspectThree variable within the function 'declareAllSuspects' 
-is not accessable globally, and therefore, the second call to the 'suspectThree' variable
+is not accessible globally, and therefore, the second call to the 'suspectThree' variable
 only returns the value from the initial, global variable.
 */
 
@@ -118,29 +118,129 @@ In this case, the key-values of a constant object can be changed, so the functio
 
 // Episode 6 
 
-let murderer = 'Colonel Mustard';
+// let murderer = 'Colonel Mustard';
 
-const changeMurderer = function() {
-  murderer = 'Mr. Green';
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
 
-  const plotTwist = function() {
-    murderer = 'Mrs. White';
-  }
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
 
-  plotTwist();
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+/* Prediction - "The murderer is Mrs.White."
+Within the 'changeMurderer' and 'plotTwist' functions, the variable murderer is assigned
+without a variable type declaration, therefore it is globally accessible
+*/
+
+// Episode 7 
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+/* Prediction = "The murderer is Mr Green"
+The initial 'let murderer' is overwritten by the declaration within 'changeMurderer', which 
+has no variable type declaration. The next murderer declaration within 'plotTwist' would over-
+write this, but as it is a 'let' type declaration, it's not accessable outside of it's function
+or it's nested function.  
+*/
+
+// Episode 8
+
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+// const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+// }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+/* Prediction - "The weapon is Candle Stick."
+In 'changeScenario' the room value is changed to the string 'Dining Room', which is 
+also the string passed as an argument to the nested 'plotTwist' function. This function 
+checks the argument against the room change in 'changeScenario'. We then change the murderer 
+value to the string 'Colonel Mustard', which is also the string passed as an argument to the 
+'unexpectedOutcome' function. This function checks the argument is the same as the value held
+in the murderer key of the scenario object and then changes the weapon to "Candle Stick".
+*/
+
+// Epsiode 9 
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
 }
 
-const declareMurderer = function () {
+const declareMurderer = function() {
   return `The murderer is ${murderer}.`;
 }
 
-changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
 
-/* Prediction - "The murderer is Mrs.White."
-
+/* Prediction - "The murderer is Professor Plum"
+The if statement sets murderer to 'Mrs Peacock', but only within the if statement block.
 */
-
 
 
